0b5531375983d7c76c9eb421a5c0ad6502e8740b
Alexandr Donskov <mrad@list.ru>
2022-04-07 17:48:30 +0300

    added Web test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,9 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import tests.helpers.Attach;
 import org.aeonbits.owner.Config;
@@ -24,9 +27,45 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = String.valueOf(Project.isRemoteWebDriver());
 
-       }
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        OperaOptions operaOptions = new OperaOptions();
+        switch (Configuration.browser) {
+            case "chrome":
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-infobars");
+                chromeOptions.addArguments("--disable-popup-blocking");
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--lang=en-en");
+                capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+                break;
+            case "firefox":
+                firefoxOptions.addArguments("--fast-start");
+                firefoxOptions.addArguments("--enable-logging");
+                firefoxOptions.addArguments("--ignore-certificate-errors");
+                firefoxOptions.addArguments("--disable-gpu");
+                capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, firefoxOptions);
+                break;
+            case "opera":
+                operaOptions.addArguments("--disable-gpu");
+                capabilities.setCapability(OperaOptions.CAPABILITY, operaOptions);
+                break;
+        }
+
+        if (Project.isRemoteWebDriver()) {
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.remote = Project.webConfig.remoteUrl();
+        }
+
+        Configuration.browserCapabilities = capabilities;
+
+    }
+//        Configuration.remote = String.valueOf(Project.isRemoteWebDriver());
+
+
 
     @AfterEach
     void addAttachments() {
